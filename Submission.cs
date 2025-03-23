5e08c660b12268e4c62b4120d330dd0a6cddb592
ace21ad <alastairdobie@gmail.com>
2025-03-23 03:56:49 +0000

    Addition: Added submission logic to submission

--- Submission.cs
+++ Submission.cs
@@ -1,8 +1,51 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
 public partial class Submission : Area2D
 {
+
+    private Document _submittedDocument;
+
+    public void _on_submission_entered(Node2D body) {
+        if (body is Document document) 
+        {
+            document.ConstantForce = new Vector2(0,0);
+            _submittedDocument = document;
+            var response = DocumentEvaluator.EvaluateDocument(document);
+            switch (response.Success) {
+                case ValidationResult.FAIL:
+
+                    giveFeedback(response.Issue);
+                    returnForm();
+                    break;
+
+                case ValidationResult.MISTAKE:
+
+                    giveFeedback(response.Issue);
+                    returnForm();
+                    break;
+
+                case ValidationResult.WIN:
+                    giveFeedback("The waiting list is 25 years long, so good luck!");
+                    break;
+            } 
+            
+        }
+    }
+
+
+    public void giveFeedback( String issue) {
+        DialogHandler.Instance.ShowDialog("This looks almost perfect...");
+        DialogHandler.Instance.ShowDialog("There is only one thing...");
+        DialogHandler.Instance.ShowDialog(issue);
+    }
+
+    public void returnForm() {
+       _submittedDocument.ConstantForce = (new Vector2(0,10000));
+    }
+
+
 
     public void _on_body_entered(Node2D body) {
         if (body is Document document) {
@@ -13,6 +56,7 @@ public partial class Submission : Area2D
     public void _on_body_exited(Node2D body) {
         if (body is Document document) {
             document.ConstantForce = (new Vector2(0,0));
+            
         }
     }
 }
