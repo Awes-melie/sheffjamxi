63f707a614570ae68a654ad0dd950b846bfbf3c4
ace21ad <alastairdobie@gmail.com>
2025-03-23 06:01:07 +0000

    Fix: Adjusted Values and size of form

--- CoordinateUtilities.cs
+++ CoordinateUtilities.cs
@@ -4,6 +4,6 @@ public class CoordinateUtilities
 {
     public static Vector2I WorldToTexture(Vector2 point)
     {
-        return (Vector2I)((point + new Vector2(230.4f, 345.6f))*1.72f);
+        return (Vector2I)((point + new Vector2(198.5f, 280.5f)));
     }
 }
